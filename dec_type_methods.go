8ad70aeeee6f802d46b4c229b12d25043979ff3d
nicolito128 <nicolito128@hotmail.com>
2024-08-03 04:20:24 -0300

    update: docs and type methods

--- dec_type_methods.go
+++ dec_type_methods.go
@@ -2,10 +2,174 @@ package goetf
 
 import (
 	"encoding/binary"
-	"fmt"
+	"math"
+	"reflect"
 )
 
-// readType reads a specific tag type from the underlying buffer,
+// parseStaticType parses a specific tag type from the input data
+// and then returns the parsed value in the form of any.
+func (d *Decoder) parseStaticType(kind reflect.Kind, tag ExternalTagType, data []byte) any {
+	switch tag {
+	case EttNil:
+		return nil
+
+	case EttString:
+		switch kind {
+		default:
+			return d.parseString(data)
+		case reflect.Slice:
+			return data
+		}
+
+	case EttAtom, EttAtomUTF8, EttSmallAtom, EttSmallAtomUTF8:
+		s := d.parseString(data)
+		switch {
+		case s == "true":
+			return true
+		case s == "false":
+			return false
+		case s == "nil":
+			return nil
+		}
+
+		return d.cache.Deduplicate(s)
+
+	case EttSmallInteger:
+		switch kind {
+		default:
+			return d.parseSmallInteger(data)
+		case reflect.Int:
+			return int(d.parseSmallInteger(data))
+		case reflect.Int32:
+			return int32(d.parseSmallInteger(data))
+		case reflect.Int64:
+			return int64(d.parseSmallInteger(data))
+		case reflect.Uint:
+			return uint(d.parseSmallInteger(data))
+		case reflect.Uint16:
+			return uint16(d.parseSmallInteger(data))
+		case reflect.Uint32:
+			return uint32(d.parseSmallInteger(data))
+		case reflect.Uint64:
+			return uint64(d.parseSmallInteger(data))
+		}
+
+	case EttInteger:
+		switch kind {
+		default:
+			return d.parseInteger(data)
+		case reflect.Int:
+			return int(d.parseInteger(data))
+		case reflect.Int64:
+			return int64(d.parseInteger(data))
+		}
+
+	case EttNewFloat:
+		switch kind {
+		default:
+			return d.parseNewFloat(data)
+		case reflect.Float32:
+			return float32(d.parseNewFloat(data))
+		}
+
+	case EttFloat:
+		switch kind {
+		default:
+			return d.parseFloat(data)
+		case reflect.Float32:
+			return float32(d.parseFloat(data))
+		}
+
+	case EttSmallBig:
+		switch kind {
+		default:
+			return d.parseSmallBig(data)
+		case reflect.Int:
+			return int(d.parseSmallBig(data))
+		}
+
+	case EttLargeBig:
+		switch kind {
+		default:
+			return d.parseLargeBig(data)
+		case reflect.Int:
+			return int(d.parseLargeBig(data))
+		}
+
+	case EttBinary:
+		switch kind {
+		default:
+			return data
+		case reflect.String:
+			return string(data)
+		}
+
+	case EttBitBinary:
+		switch kind {
+		default:
+			return data
+		case reflect.String:
+			return string(data)
+		}
+	}
+
+	return nil
+}
+
+func (d *Decoder) parseString(b []byte) string {
+	return string(b)
+}
+
+func (d *Decoder) parseSmallInteger(b []byte) uint8 {
+	return uint8(b[0])
+}
+
+func (d *Decoder) parseInteger(b []byte) int32 {
+	return int32(binary.BigEndian.Uint32(b))
+}
+
+func (d *Decoder) parseNewFloat(b []byte) float64 {
+	bits := binary.BigEndian.Uint64(b)
+	float := math.Float64frombits(bits)
+	return float
+}
+
+func (d *Decoder) parseFloat(b []byte) float64 {
+	// todo: change to use string float IEEE format
+	bits := binary.LittleEndian.Uint64(b)
+	float := math.Float64frombits(bits)
+	return float
+}
+
+func (d *Decoder) parseSmallBig(b []byte) int64 {
+	sign := b[0]
+	rest := b[1:]
+
+	bits := binary.LittleEndian.Uint64(rest)
+	smallBig := int64(bits)
+
+	if sign == 1 {
+		smallBig *= -1
+	}
+
+	return smallBig
+}
+
+func (d *Decoder) parseLargeBig(b []byte) int64 {
+	sign := b[0]
+	rest := b[1:]
+
+	bits := binary.LittleEndian.Uint64(rest)
+	largeBig := int64(bits)
+
+	if sign == 1 {
+		largeBig *= -1
+	}
+
+	return largeBig
+}
+
+// readStaticType reads a specific tag type from the underlying buffer,
 // then returns the number of bytes read, a byte slice and an error, if any.
 func (d *Decoder) readStaticType(tag ExternalTagType) (n int, b []byte, err error) {
 	switch tag {
@@ -133,8 +297,6 @@ func (d *Decoder) readLargeBig() (int, []byte, error) {
 	largeBig := make([]byte, N+1)
 	largeBig[0] = sign
 	copy(largeBig[1:], data)
-
-	fmt.Println(largeBig)
 
 	return len(largeBig) - 1, largeBig, nil
 }
