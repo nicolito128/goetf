5d7c66493c0aeeafb464a40ce135690f4e6e735e
nicolito128 <nicolito128@hotmail.com>
2024-08-04 04:52:58 -0300

    fix typo

--- examples/marshal/main.go
+++ examples/marshal/main.go
@@ -8,7 +8,6 @@ import (
 
 func main() {
 	phrase := "\"Real stupidity beats artificial intelligence every time.\" - Terry Pratchett, Hogfather"
-	println(len(phrase))
 
 	data, err := goetf.Marshal(phrase)
 	if err != nil {
